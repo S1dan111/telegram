9665c5a5b05ca25520e247e41aa92e8e0b3775ad
S1dan111 <sidan7481@gmail.com>
2025-11-23 15:51:51 +0300

    Update bot.tg.py

--- bot.tg.py
+++ bot.tg.py
@@ -1,30 +1,48 @@
-import random
+import random,telebot,sqlite3
+from random import randint
 
-import  telebot
+import seconds
+from telebot import TeleBot
+
 
 token = '8470143163:AAHOFKNTjkqG46cV4jHtfHlU11-E5GuHGPg'
 bot = telebot.TeleBot(token)
 
 
-Sovet_mem= [
-    "Главный мем года - 67"
-    "Тут вам не там,там вам не тут"
-    "Я тут босс"
-    "Главное думать только о себе"
-    "Хочешь трудиться - иди трудись"
-    "Андроид и Айфон- 2 разные вещи"
-    "Нельзя сравнить макбук и виндовс"
-    "у виво лучшие шайбы"
-]
-
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(message.chat.id, 'привет я бот помощник')
+    conn = sqlite3.connect("tgbot.sql")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS users (id int auto_increment primary key,name varchar(50), pass varchar(50))")
 
-@bot.message_handler(commands=['sovet'])
-def sovet(message):
-    bot.send_message(message.chat.id, random.choice(Sovet_mem))
+    conn.commit()
+    cur.close()
+    conn.close()
+    bot.send_message(message.chat.id,"Сейчас тебя зарегестрируем! Введите свое имя")
+    bot.register_next_step_handler(message,user_name)
 
 
+def user_name(message):
+    global name
+    name = message.text.strip()
+    bot.send_message(message.chat.id,"Введите свой пароль")
+    bot.register_next_step_handler(message,user_pass)
+
+
+def user_pass(message):
+    password = hash(message.text.strip())
+    bot.send_message(message.chat.id,"Отлично,ты зарегестрирован")
+
+
+    conn = sqlite3.connect("tgbot.sql")
+    cur = conn.cursor()
+
+    cur.execute('INSERT INTO users (name,pass) VALUES (\'%s\',\'%s\')' % (name, password))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    print(name, password)
 
 bot.polling()
